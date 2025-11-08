48e85654ec135cff9fcf2f80c3aeca14807a62ae
LuscaLisboa <lglisboa1503@gmail.com>
2025-11-07 23:27:04 -0300

    setup - logging

--- Engine/src/Engine/EntryPoint.h
+++ Engine/src/Engine/EntryPoint.h
@@ -5,6 +5,11 @@
 extern Engine::Application* Engine::CreateApplication();
 
 int main(int argc, char** argv){
+
+    Engine::Log::Init();
+    ENGINE_CORE_WARN("Core Log Initialized");
+    ENGINE_INFO("Client Log Initialized");
+
     auto app = Engine::CreateApplication();
     app->Run();
     delete app;
