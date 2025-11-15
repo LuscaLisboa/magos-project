88c966b0ffee429bee7cedec163131fbac21d2f1
LuscaLisboa <lglisboa1503@gmail.com>
2025-11-15 20:07:11 -0300

    input - polling

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -3,6 +3,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Engine {
     std::string_view GetConfig(){
         return kEngineConfig;
@@ -55,6 +57,10 @@ namespace Engine {
             for(Layer* layer : m_LayerStack) {
                 layer->OnUpdate();
             }
+
+            auto[x, y] = Input::GetMousePosition();
+            ENGINE_CORE_TRACE("{0}, {1}", x, y);
+
             m_Window->OnUpdate();
         }
     }
--- Engine/src/Engine/ImGui/ImGuiLayer.h
+++ Engine/src/Engine/ImGui/ImGuiLayer.h
@@ -6,6 +6,8 @@
 #include "Engine/Events/KeyEvent.h"
 #include "Engine/Events/MouseEvents.h"
 
+struct ImGuiIO;
+
 namespace Engine {
 
     class ENGINE_API ImGuiLayer : public Layer {
@@ -28,7 +30,18 @@ namespace Engine {
         bool OnKeyTypedEvent(KeyTypedEvent& e);
         bool OnWindowResizeEvent(WindowResizeEvent& e);
 
+        void UpdateModifierKeys(ImGuiIO& io);
+
         float m_Time = 0.0f;
+
+        bool m_LeftCtrlPressed = false;
+        bool m_RightCtrlPressed = false;
+        bool m_LeftShiftPressed = false;
+        bool m_RightShiftPressed = false;
+        bool m_LeftAltPressed = false;
+        bool m_RightAltPressed = false;
+        bool m_LeftSuperPressed = false;
+        bool m_RightSuperPressed = false;
     };   
 
 }
